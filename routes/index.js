b4036da32385a4422d6faa233730fc03a13eb6f3
maiko254 <bonyomichael406.com>
2024-09-16 12:26:12 +0300

    feat: Created endpoints to create, fetch and filter property and apartment data

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import AppController from '../controllers/AppController.js';
 import UsersController from '../controllers/UsersController.js';
 import AuthController from '../controllers/AuthController.js';
+import PropertyController from '../controllers/PropertyController.js';
 
 const router = Router();
 
@@ -10,5 +11,13 @@ router.post('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
+router.post('/properties', PropertyController.createProperty);
+router.get('/properties', PropertyController.getProperties);
+router.get('/properties/:id', PropertyController.getPropertyById);
+router.put('/properties/:id', PropertyController.updateProperty);
+router.delete('/properties/:id', PropertyController.deleteProperty);
+router.get('/properties/user/:id', PropertyController.getPropertyByOwnerId);
+router.post('/properties/:id/apartments', PropertyController.createApartment);
+router.get('/properties/:id/apartments', PropertyController.getApartments);
 
 export default router;
